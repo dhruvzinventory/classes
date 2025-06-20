d00088d67414c21103c2dbcdcf473815a197059a
Dhruv Shukla <142580257+dhruvzinventory@users.noreply.github.com>
2025-06-20 12:02:07 +0530

    Add files via upload

---
+++ classesApp.swift
@@ -0,0 +1,17 @@
+//
+//  classesApp.swift
+//  classes
+//
+//  Created by Dhruv Shukla on 19/06/25.
+//
+
+import SwiftUI
+
+@main
+struct classesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
